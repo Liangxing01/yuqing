fecfbfd6e6d59183136fcf868dee70a58bd9b2ef
Roshomon <14833538@qq.com>
2016-10-26 11:13:47 +0800

    修改error目录

--- application/config/smarty.php
+++ application/config/smarty.php
@@ -3,7 +3,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config['cache_lifetime'] = 60;
 $config['caching'] = false;
 $config['template_dir'] = APPPATH .'views';
-$config['compile_dir'] = APPPATH .'views/template_c';
+$config['compile_dir'] = '/template_c';
 $config['cache_dir'] = APPPATH . 'views/cache';
 $config['config_dir'] = APPPATH . 'views/config';
 $config['use_sub_dirs'] = false; //子目录变量(是否在缓存文件夹中生成子目录)
