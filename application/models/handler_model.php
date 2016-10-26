cb43120d35ceb27b9fc3ed0c6f20a54d31e80a2d
Roshomon <14833538@qq.com>
2016-10-26 21:32:37 +0800

    待处理，正在处理列表

--- application/models/handler_model.php
+++ application/models/handler_model.php
@@ -6,20 +6,27 @@
         }
 
         //查询所有待处理事件
-        public function get_all_unhandle($processorID,$per,$offset){
+        public function get_all_unhandle($pInfo,$processorID){
             //多表联合查询
-            $sql  = "SELECT a.id,a.time,a.description,c.username as processor,d.username as zpname,b.title FROM
-(SELECT * FROM yq_event_designate  WHERE processor = 3 ) as a JOIN yq_event as b on a.event_id = b.id
-JOIN yq_user as c on c.id = a.processor JOIN yq_user AS d on d.id = a.manager WHERE b.state = \"已指派\";";
+            $sql  = "SELECT a.id,b.title,t.name as type_name ,a.time,a.description,d.username as zpname FROM
+(SELECT * FROM yq_event_designate  WHERE processor = ? ) as a JOIN yq_event as b on a.event_id = b.id
+JOIN yq_user AS d on d.id = a.manager JOIN yq_event_type AS t on b.type = t.id WHERE b.state = \"已指派\" limit ?,?;";
 
-            $sql2 = "SELECT a.id,a.time,a.description,c.username as processor,d.username as zpname,b.title FROM
+            $sql2 = "SELECT a.id,b.title,t.name as type_name,a.time,a.description,d.username as zpname FROM
 (SELECT * FROM yq_event_designate  WHERE processor = 3 ) as a JOIN yq_event as b on a.event_id = b.id
-JOIN yq_user as c on c.id = a.processor JOIN yq_user AS d on d.id = a.manager WHERE b.state = \"已指派\";";
-            $data = $this->db->query($sql,array($offset,$per))->result_array();
-            $data['total'] = count($this->db->query($sql2)->result_array());
+JOIN yq_user AS d on d.id = a.manager JOIN yq_event_type AS t on b.type = t.id WHERE b.state = \"已指派\";";
+
+
+            $data['aaData'] = $this->db->query($sql,array($processorID,0,10))->result_array();
+            $total = $this->db->query($sql2,array($processorID))->num_rows();
+            $data['sEcho']                = $pInfo['sEcho'];
+
+            $data['iTotalDisplayRecords'] = $total;
+
+            $data['iTotalRecords']        = $total;
 
             if(!empty($data)){
-                return $data[0];
+                return $data;
             }else{
                 return false;
             }
@@ -28,6 +35,31 @@ JOIN yq_user as c on c.id = a.processor JOIN yq_user AS d on d.id = a.manager WH
         //根据用户关键字查询待处理事件
         public function get_unhandle_by_keyword($key){
             $this->db->where('');
+        }
+
+        //查询正在处理事件
+        public function get_doing_handle($pInfo,$processorID){
+            $sql = "SELECT a.id,b.title,t.name as type_name, u.username as zpname,a.time,a.state FROM
+(SELECT * from  yq_event_log WHERE processor = ? and state ='处理中' and ISNULL(pid)) as a
+JOIN yq_event as b on a.event_id = b.id JOIN yq_event_type as t on b.type = t.id JOIN yq_user as u on a.manager = u.id limit ?,?";
+
+            $sql2 = "SELECT a.id,b.title,t.name as type_name, u.username as zpname,a.time,a.state FROM
+(SELECT * from  yq_event_log WHERE processor = ? and state ='处理中' and ISNULL(pid)) as a
+JOIN yq_event as b on a.event_id = b.id JOIN yq_event_type as t on b.type = t.id JOIN yq_user as u on a.manager = u.id";
+
+            $data['aaData'] = $this->db->query($sql,array($processorID,0,10))->result_array();
+            $total = $this->db->query($sql2,array($processorID))->num_rows();
+            $data['sEcho']                = $pInfo['sEcho'];
+
+            $data['iTotalDisplayRecords'] = $total;
+
+            $data['iTotalRecords']        = $total;
+
+            if(!empty($data)){
+                return $data;
+            }else{
+                return false;
+            }
         }
 
 
