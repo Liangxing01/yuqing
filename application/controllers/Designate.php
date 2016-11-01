d2e9f5e76d3e4d853bfd891285b5266d271adc53
Code with ❤️ by Rei <158686785@qq.com>
2016-11-01 17:45:20 +0800

    信息确认 完成

--- application/controllers/Designate.php
+++ application/controllers/Designate.php
@@ -14,6 +14,97 @@ class Designate extends MY_controller
 
 
     /**
+     * 未处理信息 视图载入
+     */
+    public function info_not_handle()
+    {
+        $this->assign("active_title", "designate_parent");
+        $this->assign("active_parent", "info_not_handle");
+        $this->all_display("designate/info_not_handle.html");
+    }
+
+
+    /**
+     * 未处理信息列表分页 数据接口
+     */
+    public function info_not_handle_data()
+    {
+        $pData['sEcho'] = $this->input->post('psEcho', true);           //DataTables 用来生成的信息
+        $pData['start'] = $this->input->post('iDisplayStart', true);    //显示的起始索引
+        $pData['length'] = $this->input->post('iDisplayLength', true);  //每页显示的行数
+        $pData['sort_th'] = $this->input->post('iSortCol_0', true);     //排序的列 默认第三列
+        $pData['sort_type'] = $this->input->post('sSortDir_0', true);   //排序的方向 默认 desc
+        $pData['search'] = $this->input->post('sSearch', true);         //全局搜索关键字 默认为空
+
+        if ($pData['start'] == NULL) {
+            $pData['start'] = 0;
+        }
+        if ($pData['length'] == NULL) {
+            $pData['length'] = 10;
+        }
+        if ($pData["sort_th"] == NULL) {
+            $pData["sort_th"] = 2;
+        }
+        if ($pData['sort_type'] == NULL) {
+            $pData['sort_type'] = "desc";
+        }
+        if ($pData['search'] == NULL) {
+            $pData['search'] = '';
+        }
+
+        $this->load->model("Designate_Model", "designate");
+        $data = $this->designate->info_not_handle_pagination($pData);
+
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
+    }
+
+
+    /**
+     * 舆情信息详情页面 视图载入
+     */
+    public function info_detail()
+    {
+        $info_id = $this->input->get("id", true);
+        if (!isset($info_id) || $info_id == null || $info_id == "") {
+            show_404();
+        }
+
+        $this->load->model("Designate_Model", "designate");
+        //设置信息为已查看状态
+        $this->designate->set_info_seen($info_id);
+        $info = $this->designate->get_info($info_id);
+        $type_list = $this->designate->get_info_type();
+        $this->assign("info", $info);
+        $this->assign("type", $type_list);
+        $this->assign("active_title", "designate_parent");
+        $this->assign("active_parent", "info_not_handle");
+        $this->all_display("designate/info_detail.html");
+    }
+
+
+    /**
+     * 提交 信息确认
+     */
+    public function commit_info()
+    {
+        $data = $this->input->post();
+        if (!isset($data["id"]) || $data["id"] == "") {
+            show_404();
+        }
+
+        $this->load->model("Designate_Model", "designate");
+        $result = $this->designate->info_commit($data);
+        if ($result) {
+            echo "提交成功";
+        } else {
+            echo "提交失败";
+        }
+    }
+
+
+    /**
      * 待指派事件列表 视图载入
      */
     public function event_not_designate()
@@ -114,7 +205,8 @@ class Designate extends MY_controller
     /**
      * 已指派事件列表分页 数据接口
      */
-    public function event_is_designate_data(){
+    public function event_is_designate_data()
+    {
         $pData['sEcho'] = $this->input->post('psEcho', true);           //DataTables 用来生成的信息
         $pData['start'] = $this->input->post('iDisplayStart', true);    //显示的起始索引
         $pData['length'] = $this->input->post('iDisplayLength', true);  //每页显示的行数
@@ -150,7 +242,8 @@ class Designate extends MY_controller
     /**
      * 事件追踪 视图载入
      */
-    public function event_tracer(){
+    public function event_tracer()
+    {
         $this->assign("active_title", "designate_parent");
         $this->assign("active_parent", "event_is_designate");
         $this->all_display("designate/event_tracer.html");
@@ -171,7 +264,8 @@ class Designate extends MY_controller
     /**
      * 事件检索列表分页 数据接口
      */
-    public function event_search_data(){
+    public function event_search_data()
+    {
         $pData['sEcho'] = $this->input->post('psEcho', true);           //DataTables 用来生成的信息
         $pData['start'] = $this->input->post('iDisplayStart', true);    //显示的起始索引
         $pData['length'] = $this->input->post('iDisplayLength', true);  //每页显示的行数
@@ -204,6 +298,7 @@ class Designate extends MY_controller
     }
 
 
+    //demo
     public function tree()
     {
         $this->load->model("Designate_Model", "designate");
--- application/models/Designate_Model.php
+++ application/models/Designate_Model.php
@@ -12,6 +12,84 @@ class Designate_Model extends CI_Model
 
 
     /**
+     * 待处理信息 分页数据
+     * @param $pInfo
+     * @return array
+     */
+    public function info_not_handle_pagination($pInfo)
+    {
+        $data['aaData'] = $this->db->select("info.id, info.title, source, user.name AS publisher, time")
+            ->from("info")
+            ->join("user", "user.id = info.publisher", "left")
+            ->where_not_in("select info_id from yq_event_info")
+            ->limit(10, 0)
+            ->get()->result_array();
+
+        //查询总记录条数
+        $total = $this->db->from("info")
+            ->where_not_in("select info_id from yq_event_info")
+            ->get()->num_rows();
+
+        $data['sEcho'] = $pInfo['sEcho'];
+
+        $data['iTotalDisplayRecords'] = $total;
+
+        $data['iTotalRecords'] = $total;
+
+        return $data;
+    }
+
+
+    /**
+     * 查询 舆情信息 详情
+     * @param $info_id
+     * @return array
+     */
+    public function get_info($info_id)
+    {
+        return $this->db->select("info.id, info.title, info.description, info.type, info.url, info.picture, info.source, user.name AS publisher, info.time")
+            ->from("info")
+            ->join("user", "user.id = info.publisher", "left")
+            ->where(array("info.id" => $info_id))
+            ->get()->row_array();
+    }
+
+
+    /**
+     * 设置 舆情信息 为已看状态
+     * @param $info_id
+     * @return bool
+     */
+    public function set_info_seen($info_id)
+    {
+        return $this->db->set(array("state" => 1))->where("id", $info_id)->update("info");
+    }
+
+
+    /**
+     * 舆情信息 确认
+     * @param $data
+     * @return bool
+     */
+    public function info_commit($data)
+    {
+        return $this->db->set(array("type" => $data["type"], "duplicate" => $data["duplicate"]))
+            ->where(array("id" => $data["id"]))
+            ->update("info");
+    }
+
+
+    /**
+     * 获取 信息类型
+     * @return array
+     */
+    public function get_info_type()
+    {
+        return $this->db->get("type")->result_array();
+    }
+
+
+    /**
      * 待指派事件 分页数据
      * @param $pInfo
      * @return mixed
@@ -19,25 +97,25 @@ class Designate_Model extends CI_Model
     public function event_not_designate_pagination($pInfo)
     {
         $data['aaData'] = $this->db->select("event.id, event.title, source, user.name AS publisher, start_time")
-                        ->from("event")
-                        ->join("user", "user.id = event.publisher", "left")
-                        ->where(array("state" => "已上报"))
-                        ->limit(10, 0)
-                        ->get()->result_array();
+            ->from("event")
+            ->join("user", "user.id = event.publisher", "left")
+            ->where(array("state" => "已上报"))
+            ->limit(10, 0)
+            ->get()->result_array();
 
         //查询总记录条数
         $total = $this->db->select("event.id, event.title, event_type.name AS type, source, user.username AS publisher, start_time")
-                        ->from("event")
-                        ->join("user", "user.id = event.publisher", "left")
-                        ->join("event_type", "event_type.id = event.type", "left")
-                        ->where(array("state" => "已上报"))
-                        ->get()->num_rows();
+            ->from("event")
+            ->join("user", "user.id = event.publisher", "left")
+            ->join("event_type", "event_type.id = event.type", "left")
+            ->where(array("state" => "已上报"))
+            ->get()->num_rows();
 
-        $data['sEcho']                = $pInfo['sEcho'];
+        $data['sEcho'] = $pInfo['sEcho'];
 
         $data['iTotalDisplayRecords'] = $total;
 
-        $data['iTotalRecords']        = $total;
+        $data['iTotalRecords'] = $total;
 
         return $data;
 
@@ -66,11 +144,11 @@ class Designate_Model extends CI_Model
             ->where(array("state" => "已指派"))
             ->get()->num_rows();
 
-        $data['sEcho']                = $pInfo['sEcho'];
+        $data['sEcho'] = $pInfo['sEcho'];
 
         $data['iTotalDisplayRecords'] = $total;
 
-        $data['iTotalRecords']        = $total;
+        $data['iTotalRecords'] = $total;
 
         return $data;
 
@@ -95,11 +173,11 @@ class Designate_Model extends CI_Model
         //查询总记录条数
         $total = $this->db->from("event")->get()->num_rows();
 
-        $data['sEcho']                = $pInfo['sEcho'];
+        $data['sEcho'] = $pInfo['sEcho'];
 
         $data['iTotalDisplayRecords'] = $total;
 
-        $data['iTotalRecords']        = $total;
+        $data['iTotalRecords'] = $total;
 
         return $data;
 
@@ -111,7 +189,8 @@ class Designate_Model extends CI_Model
      * @param $event_id
      * @return mixed
      */
-    public function get_event_info($event_id){
+    public function get_event_info($event_id)
+    {
         $event = $this->db->select("event.id, event.title, event.url, event.description, event.picture, event_type.name AS type, source, user.name AS publisher, start_time")
             ->from("event")
             ->join("user", "user.id = event.publisher", "left")
@@ -123,7 +202,8 @@ class Designate_Model extends CI_Model
     }
 
 
-    public function get_relation_tree(){
+    public function get_relation_tree()
+    {
         $root = $this->db->query("SELECT lft, rgt FROM yq_relation WHERE name='组织关系'")->first_row('array');
 
         // 以一个空的$right栈开始
@@ -157,12 +237,13 @@ class Designate_Model extends CI_Model
      * @param $data
      * @return mixed
      */
-    public function event_designate($data){
+    public function event_designate($data)
+    {
         $processors = explode(",", $data["processor"]);
         $insert = array();
         $time = time();
         $manager_id = $this->session->userdata("uid");
-        foreach( $processors AS $processor_id ){
+        foreach ($processors AS $processor_id) {
             $insert[] = array(
                 "event_id" => $data["event_id"],
                 "description" => $data["description"],
@@ -173,7 +254,7 @@ class Designate_Model extends CI_Model
         }
 
         //TODO 检测是否重复指派
-        $result = $this->db->where("id", $data["event_id"])->update("event", array("state"=>"已指派"));
+        $result = $this->db->where("id", $data["event_id"])->update("event", array("state" => "已指派"));
 
         return $this->db->insert_batch("event_designate", $insert);
     }
