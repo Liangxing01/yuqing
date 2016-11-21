0f7850da59eb8e6b8fe138544fc8799355a5b82b
Code with ❤️ by Rei <158686785@qq.com>
2016-11-21 21:30:25 +0800

    fix

--- application/controllers/Designate.php
+++ application/controllers/Designate.php
@@ -270,7 +270,7 @@ class Designate extends MY_controller
         $uid = $this->session->userdata('uid');
 
         //判断有无督办权限
-        $this->load->model("My_Model", "my_model");
+        $this->load->model("MY_Model", "my_model");
         $duban = $this->my_model->check_duban($uid,$event_id);
         if ($duban){
             $usertype = 1;
