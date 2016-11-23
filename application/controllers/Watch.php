59b51f98804c9394aea456f4737dab3729af733d
Rei <8akarei@gmail.com>
2016-11-23 13:43:51 +0800

    控制器增加登陆验证

--- application/controllers/Watch.php
+++ application/controllers/Watch.php
@@ -11,6 +11,7 @@ class Watch extends MY_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model('Watcher_Model','watcher_model');
+        $this->identity->is_authentic();
     }
 
     /**
