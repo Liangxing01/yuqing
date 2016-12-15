7a9f2cc7d570dbd2f312c86cd3aeb04db7d3a210
MaskerJ <14833538@qq.com>
2016-12-15 21:48:04 +0800

    新增 DNS IP 管控

--- application/controllers/Yq_control.php
+++ application/controllers/Yq_control.php
@@ -33,10 +33,15 @@ class Yq_control extends MY_Controller {
         $con_url = $this->input->get('control_url');
         $redi_url = $this->input->get('redirect_url');
         $domain  = $this->input->get('domain');
+        $alias  =  $this->input->get('alias');
 
         //管控该url
-        $res = $this->url_control->ctl_url($domain,$con_url,$redi_url);
-        echo $res;
+        $res = $this->url_control->ctl_url($domain,$con_url,$redi_url,$alias);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
     }
 
     /**
@@ -46,7 +51,11 @@ class Yq_control extends MY_Controller {
     public function del_rule(){
         $rid = $this->input->get('rule_id');
         $res = $this->url_control->del_url($rid);
-        var_dump($res);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
     }
 
 
@@ -87,8 +96,120 @@ class Yq_control extends MY_Controller {
      * 接口：DNS 管控
      */
     public function dns_ctl(){
-        
+        $ctl_domain = $this->input->get('ctl_domain');
+        $alias = $this->input->get('alias');
+        $res = $this->url_control->ctl_dns($ctl_domain,$alias);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
     }
+
+    /**
+     * 删除 DNS
+     */
+    public function del_dns(){
+        $rid = $this->input->get('rule_id');
+        $res = $this->url_control->del_dns($rid);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
+    }
+
+    public function show_dns_list(){
+        $pData['sEcho'] = $this->input->post('psEcho', true);           //DataTables 用来生成的信息
+        $pData['start'] = $this->input->post('iDisplayStart', true);    //显示的起始索引
+        $pData['length'] = $this->input->post('iDisplayLength', true);  //每页显示的行数
+        $pData['sort_th'] = $this->input->post('iSortCol_0', true);     //排序的列 默认第三列
+        $pData['sort_type'] = $this->input->post('sSortDir_0', true);   //排序的方向 默认 desc
+        $pData['search'] = $this->input->post('sSearch', true);         //全局搜索关键字 默认为空
+
+        $pData['rank'] = $this->input->post('rank');
+
+        if ($pData['start'] == NULL) {
+            $pData['start'] = 0;
+        }
+        if ($pData['length'] == NULL) {
+            $pData['length'] = 10;
+        }
+        if ($pData["sort_th"] == NULL) {
+            $pData["sort_th"] = 2;
+        }
+        if ($pData['sort_type'] == NULL) {
+            $pData['sort_type'] = "desc";
+        }
+        if ($pData['search'] == NULL) {
+            $pData['search'] = '';
+        }
+
+        $data = $this->url_control->show_ctl_dns($pData);
+        echo json_encode($data);
+    }
+
+    /**
+     * IP 管控
+     */
+    public function ctl_ip(){
+        $ctl_ip = $this->input->get('ctl_ip');
+        $alias = $this->input->get('alias');
+        $res = $this->url_control->ctl_ip($ctl_ip,$alias);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
+    }
+
+    /**
+     * 删除 IP
+     */
+    public function del_ip(){
+        $rid = $this->input->get('rule_id');
+        $res = $this->url_control->del_ip($rid);
+        if($res){
+            echo json_encode($res);
+        }else{
+            echo json_encode($res);
+        }
+    }
+
+    /**
+     * 展示 ip 列表
+     */
+    public function show_ip_list(){
+        $pData['sEcho'] = $this->input->post('psEcho', true);           //DataTables 用来生成的信息
+        $pData['start'] = $this->input->post('iDisplayStart', true);    //显示的起始索引
+        $pData['length'] = $this->input->post('iDisplayLength', true);  //每页显示的行数
+        $pData['sort_th'] = $this->input->post('iSortCol_0', true);     //排序的列 默认第三列
+        $pData['sort_type'] = $this->input->post('sSortDir_0', true);   //排序的方向 默认 desc
+        $pData['search'] = $this->input->post('sSearch', true);         //全局搜索关键字 默认为空
+
+        $pData['rank'] = $this->input->post('rank');
+
+        if ($pData['start'] == NULL) {
+            $pData['start'] = 0;
+        }
+        if ($pData['length'] == NULL) {
+            $pData['length'] = 10;
+        }
+        if ($pData["sort_th"] == NULL) {
+            $pData["sort_th"] = 2;
+        }
+        if ($pData['sort_type'] == NULL) {
+            $pData['sort_type'] = "desc";
+        }
+        if ($pData['search'] == NULL) {
+            $pData['search'] = '';
+        }
+
+        $data = $this->url_control->show_ip_list($pData);
+        echo json_encode($data);
+    }
+
+
 
     public function test(){
         $set = "http://werewr.erwer/weraw.er.aewr.awe.re";
--- application/models/URL_Model.php
+++ application/models/URL_Model.php
@@ -42,7 +42,7 @@ class URL_Model extends CI_Model {
      * @param $redi_url
      * 管控url
      */
-    public function ctl_url($domain,$ctl_url,$redi_url){
+    public function ctl_url($domain,$ctl_url,$redi_url,$alias){
         $re = "/^((http|ftp|https):\/\/)?(.*)/";
         preg_match($re, $ctl_url,$res);
         $ctl_url = $res[3];
@@ -50,19 +50,24 @@ class URL_Model extends CI_Model {
         //开始运行事务
         $this->db->trans_begin();
 
-        $has_domain = $this->check_has_domain($domain);
+        $has_domain = $this->check_has_domain($domain."_http",'http');
         $ctl_code = array();
 
         //插入管控记录
         if($has_domain){
             $this->insert_ctl_url($has_domain['id'],$ctl_url);
             $rule_id = $this->db->insert_id();
-            array_push($ctl_code,"/usr/ramdisk/app/v6plus/http_ctl.sh dns_add ".$has_domain['domain']." ".$ctl_url);
+            array_push($ctl_code,"/usr/ramdisk/app/v6plus/http_ctl.sh dns_add ".$has_domain['group_name']." ".$ctl_url);
+            array_push($ctl_code,"/usr/ramdisk/app/v6plus/http_ctl.sh urlfilter_addrule ".$rule_id." ".$has_domain['group_name']." ".$redi_url);
         }else{
             //先 插入 新的域名 获取域名id
             $new_data = array(
-                'domain'       =>$domain,
-                'redirect_url' => $redi_url
+                'value'        =>$domain, //http管制
+                'group_name'   =>$domain."_http", //群组名
+                'redirect_url' => $redi_url,
+                'type'         => "http",
+                'alias'        => $alias,
+                'time'         => time()
             );
             $d_id = $this->insert_info('ctl_domains',$new_data);
             $this->insert_ctl_url($d_id,$ctl_url);
@@ -89,10 +94,11 @@ class URL_Model extends CI_Model {
     /**
      * 查询 主域名 是否已经存在 于 域名组
      */
-    public function check_has_domain($domain){
-        $is_exist = $this->db->select('id,redirect_url,domain')
+    public function check_has_domain($domain,$type){
+        $is_exist = $this->db->select('id,redirect_url,domain,group_name')
             ->from('ctl_domains')
-            ->where('domain',$domain)
+            ->where('group_name',$domain)
+            ->where('type',$type)
             ->get()->row_array();
         if(empty($is_exist)){
             return false;
@@ -130,7 +136,7 @@ class URL_Model extends CI_Model {
         //获取域名 组名
         $data= $this->get_domain_name($rid);
         $url   = $data['ctl_url'];
-        $dname = $data['domain'];
+        $dname = $data['group_name'];
 
         $this->db->where('id',$rid);
         $res = $this->db->delete('ctl_url');
@@ -149,7 +155,7 @@ class URL_Model extends CI_Model {
      * 获取 域名组名
      */
     public function get_domain_name($rid){
-        $dname = $this->db->select('cd.domain,cu.ctl_url')
+        $dname = $this->db->select('cd.group_name,cu.ctl_url')
             ->from('ctl_url as cu')
             ->where('cu.id',$rid)
             ->join('ctl_domains as cd','cd.id = cu.domain_id')
@@ -162,16 +168,205 @@ class URL_Model extends CI_Model {
      * 分页 获取 已管控域名
      */
     public function show_all_list($pInfo){
-        $data['aaData'] = $this->db->select("cu.id,cu.ctl_url,cd.domain,cd.redirect_url,from_unixtime(cu.time) as time")
+        $data['aaData'] = $this->db->select("cu.id,cu.ctl_url,cd.value,cd.redirect_url,from_unixtime(cu.time) as time,cd.type,cd.alias")
             ->from('ctl_url as cu')
             ->join('ctl_domains as cd','cu.domain_id = cd.id')
+            ->where('cd.type','http')
             ->order_by('cu.time',$pInfo['sort_type'])
             ->limit($pInfo['length'],$pInfo['start'])
             ->get()->result_array();
 
-        $total =  $this->db->select("cu.id,cu.ctl_url,cd.domain,cd.redirect_url,from_unixtime(cu.time)")
+        $total =  $this->db->select("cu.id,cu.ctl_url,cd.value,cd.redirect_url,from_unixtime(cu.time)")
             ->from('ctl_url as cu')
             ->join('ctl_domains as cd','cu.domain_id = cd.id')
+            ->where('cd.type','http')
+            ->get()->num_rows();
+
+        $data['sEcho'] = $pInfo['sEcho'];
+
+        $data['iTotalDisplayRecords'] = $total;
+
+        $data['iTotalRecords'] = $total;
+
+        return $data;
+    }
+
+    /**
+     * --------------------------------DNS管控--------------------------------
+     */
+    /**
+     * dns管控，默认 定向到 重大
+     */
+    public function ctl_dns($ctl_domain,$alias){
+        $ip = "202.202.2.42";
+        $ctl_code = array();
+        $gname = $ctl_domain."_dns";
+        //检查有该 域名组没有
+        $is_exist = $this->check_has_domain($value,'dns');
+        if($is_exist){
+            return array(
+                'res' => 0,
+                'msg' => '该域名已DNS管控'
+            );
+        }else{
+            $info = array(
+                'value' => $ip,
+                'group_name' => $gname,
+                'type'  => "dns",
+                'time'  => time(),
+                'alias' => $alias
+            );
+            $this->db->insert('ctl_domains',$info);
+            $dns_id = $this->db->insert_id();
+
+            array_push($ctl_code,"/usr/ramdisk/app/v6plus/dns_ctl.sh dnsctl_addrule ".$dns_id." ".$gname. " ".$ip);
+            $this->run4601($ctl_code);
+        }
+
+        if($dns_id){
+            return array(
+                'res' => 1,
+                'msg' => 'DNS管控成功'
+            );
+        }else{
+            return array(
+                'res' => 0,
+                'msg' => 'DNS管控失败'
+            );
+        }
+    }
+
+    /**
+     * DNS 删除
+     */
+    public function del_dns($rule_id){
+        $this->db->where('id',$rule_id);
+        $res = $this->db->delete('ctl_domains');
+
+        //拼接命令
+        $code = array();
+        array_push($code,"/usr/ramdisk/app/v6plus/dns_ctl.sh dnsctl_rmvrule ".$rule_id);
+
+        $this->run4601($code);
+        if($res){
+            return array(
+                'res' => 1,
+                'msg' => '解除管控成功'
+            );
+        }else{
+            return array(
+                'res' => 0,
+                'msg' => '解除管控失败'
+            );
+        }
+    }
+
+    /**
+     * 分页 获取 dns 管控
+     */
+    public function show_ctl_dns($pInfo){
+        $data['aaData'] = $this->db->select("cd.id,cd.value,from_unixtime(cd.time) as time,cd.type,cd.alias")
+            ->from('ctl_domains as cd')
+            ->where('cd.type','dns')
+            ->order_by('cd.time',$pInfo['sort_type'])
+            ->limit($pInfo['length'],$pInfo['start'])
+            ->get()->result_array();
+
+        $total =  $this->db->select("cd.id,cd.value,from_unixtime(cd.time) as time,cd.type,cd.alias")
+            ->from('ctl_domains as cd')
+            ->where('cd.type','dns')
+            ->get()->num_rows();
+
+        $data['sEcho'] = $pInfo['sEcho'];
+
+        $data['iTotalDisplayRecords'] = $total;
+
+        $data['iTotalRecords'] = $total;
+
+        return $data;
+    }
+
+    /**
+     * --------------------IP管控-------------------------
+     */
+    public function ctl_ip($ip,$alias){
+        $ctl_code = array();
+        $gname = $ip."_ip";
+        //检查有该 域名组没有
+        $is_exist = $this->check_has_domain($gname,'ip');
+        if($is_exist){
+            return array(
+                'res' => 0,
+                'msg' => '该IP已经被管控'
+            );
+        }else{
+            $info = array(
+                'value' => $ip,
+                'group_name' => $gname,
+                'type'  => "ip",
+                'time'  => time(),
+                'alias' => $alias
+            );
+            $this->db->insert('ctl_domains',$info);
+            $ip_id = $this->db->insert_id();
+
+            array_push($ctl_code,"/usr/ramdisk/app/v6plus/ip_ctl.sh ipctl_addrule ".$ip_id." ".$gname);
+            $this->run4601($ctl_code);
+        }
+
+        if($ip_id){
+            return array(
+                'res' => 1,
+                'msg' => 'IP管控成功'
+            );
+        }else{
+            return array(
+                'res' => 0,
+                'msg' => 'IP管控失败'
+            );
+        }
+    }
+
+
+    /**
+     * 删除 IP 管控
+     */
+    public function del_ip($rule_id){
+        $this->db->where('id',$rule_id);
+        $res = $this->db->delete('ctl_domains');
+
+        //拼接命令
+        $code = array();
+        array_push($code,"/usr/ramdisk/app/v6plus /ip_ctl.sh ipctl_rmvrule ".$rule_id);
+
+        $this->run4601($code);
+        if($res){
+            return array(
+                'res' => 1,
+                'msg' => '解除管控成功'
+            );
+        }else{
+            return array(
+                'res' => 0,
+                'msg' => '解除管控失败'
+            );
+        }
+    }
+
+    /**
+     * 分页 IP 管控
+     */
+    public function show_ip_list($pInfo){
+        $data['aaData'] = $this->db->select("cd.id,cd.value,from_unixtime(cd.time) as time,cd.type,cd.alias")
+            ->from('ctl_domains as cd')
+            ->where('cd.type','ip')
+            ->order_by('cd.time',$pInfo['sort_type'])
+            ->limit($pInfo['length'],$pInfo['start'])
+            ->get()->result_array();
+
+        $total =  $this->db->select("cd.id,cd.value,from_unixtime(cd.time) as time,cd.type,cd.alias")
+            ->from('ctl_domains as cd')
+            ->where('cd.type','ip')
             ->get()->num_rows();
 
         $data['sEcho'] = $pInfo['sEcho'];
