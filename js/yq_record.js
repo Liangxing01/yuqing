ec8084c23abe12b6f2fe32a5e98fff0c9cd14c35
liangxing <476690457@qq.com>
2016-12-21 16:04:39 +0800

    功能完善

--- js/yq_record.js
+++ js/yq_record.js
@@ -7,7 +7,7 @@
 var page_num = 1;   //页码
 var page_total = 0 ; //总页码
 var page_length = 10;   //每页显示多少条
-var arr_all = ['全国','全部','显示全部','ASC','']; //默认初始查询
+var arr_all = ['全国','全部','显示全部','DESC','']; //默认初始查询
 //显示全部
 function sroll_ajax(type){
     if(type == 'all'){
@@ -176,10 +176,10 @@ $(function(){
     $('#sort').click(function(){
         if($(this).hasClass('active')){
             $(this).removeClass('active');
-            arr_all[3] = 'DESC';
+            arr_all[3] = 'ASC';
         }else{
             $(this).addClass('active');
-            arr_all[3] = 'ASC';
+            arr_all[3] = 'DESC';
         }
         load_who();
     })
