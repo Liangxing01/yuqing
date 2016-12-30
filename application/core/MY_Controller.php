2f536ec0df74df20058db1b7a14c45405085d0f7
MaskerJ <14833538@qq.com>
2016-12-30 21:18:22 +0800

    显示 舆情控制 功能

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -66,7 +66,7 @@ class MY_Controller extends CI_Controller
         }
 
         // 舆情管控菜单
-        if ($uid == 85 || $uid == 91) {
+        if ($uid == 9 || $uid == 10) {
             $this->ci_smarty->display('menu/menu_yuqingctl.html');
         }
 
