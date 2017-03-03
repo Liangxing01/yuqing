e6f872ea8623896e7e7edb69e7f5dc2df689696a
Code with ❤️ by Rei <158686785@qq.com>
2017-03-03 17:33:38 +0800

    fix

--- application/controllers/api/Api_common.php
+++ application/controllers/api/Api_common.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Api_Common extends CI_Controller
+class Api_common extends CI_Controller
 {
     public function __construct()
     {
