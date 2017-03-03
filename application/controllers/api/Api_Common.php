f60b1d01708f52b30987817bc63ee8064c48dc7a
Code with ❤️ by Rei <158686785@qq.com>
2017-03-03 17:21:08 +0800

    1.fix

---
+++ application/controllers/api/Api_Common.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api_Common extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->identity->m_is_authentic();
+        $this->load->model("API_Models/API_Common_Model", "common");
+    }
+
+
+    /**
+     * 移动端APP 首页数据接口
+     */
+    public function get_summary_info()
+    {
+        $result = $this->common->get_summary_data();
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($result));
+    }
+
+
+    /**
+     * 用户登出 接口
+     */
+    public function logout()
+    {
+        $this->identity->m_destroy();
+    }
+}
