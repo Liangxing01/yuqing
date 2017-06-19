d7a6971ff6181090a168cd3d7bfda457c9953451
Code with ❤️ by Rei <158686785@qq.com>
2017-06-19 16:58:17 +0800

    1.fix get_event_list  apk数据接口

--- application/models/API_Models/Event_Model.php
+++ application/models/API_Models/Event_Model.php
@@ -312,6 +312,41 @@ class Event_Model extends CI_Model
                 $this->success['total'] = $total;
                 return $this->success;
                 break;
+            case "processor_all":
+                // 处理中事件
+                $data = $this->db->select("event_designate.event_id, event.title, event.rank, event.start_time, event.end_time, user.name, event_designate.state")
+                    ->from("event_designate")
+                    ->join('event', 'event.id = event_designate.event_id', 'left')
+                    ->join('user', 'user.id = event_designate.manager', 'left')
+                    ->group_start()
+                    ->where(array("event_designate.state" => "未处理"))
+                    ->or_where(array("event_designate.state" => "处理中"))
+                    ->group_end()
+                    ->like("event.title", $keyword)
+                    ->group_start()
+                    ->where('event_designate.processor', $uid)
+                    ->or_where_in('event_designate.group', $gid)
+                    ->group_end()
+                    ->limit($size, $start)
+                    ->order_by('event_designate.time', "desc")
+                    ->get()->result_array();
+                $total = $this->db->select("event_designate.event_id")
+                    ->from("event_designate")
+                    ->join('event', 'event.id = event_designate.event_id', 'left')
+                    ->group_start()
+                    ->where(array("event_designate.state" => "未处理"))
+                    ->or_where(array("event_designate.state" => "处理中"))
+                    ->group_end()
+                    ->like("event.title", $keyword)
+                    ->group_start()
+                    ->where('event_designate.processor', $uid)
+                    ->or_where_in('event_designate.group', $gid)
+                    ->group_end()
+                    ->get()->num_rows();
+                $this->success['data'] = $data;
+                $this->success['total'] = $total;
+                return $this->success;
+                break;
             case "watcher_all":
                 $data = $this->db->select("event.id, event.title, A.name AS manager, B.name AS main_processor, C.name AS main_group, event.rank, event.state, event.start_time")
                     ->from("event")
