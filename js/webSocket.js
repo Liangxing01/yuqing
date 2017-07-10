9d3e02f8b53d6de2249a0241640ab0eaaaac1f76
maskerj <14833538@qq.com>
2017-07-10 12:10:17 +0800

    1

--- js/webSocket.js
+++ js/webSocket.js
@@ -15,7 +15,7 @@ function reload_num() {
 }
 function get_webSocket_msg() {
     try {
-        var client_socket = new WebSocket('ws://' + 'www.bnv6.com' + ':4000');
+        var client_socket = new WebSocket('ws://' + window.location.host + ':4000');
         client_socket.onopen = function () {
             console.log("服务器已连接");
         };
